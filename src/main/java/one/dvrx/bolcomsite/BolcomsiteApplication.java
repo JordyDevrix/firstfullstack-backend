2081c95d3ee71fb9d3cad1a8aa23a935e1947cb7
JordyDevrix <2109jordy2241@gmail.com>
2024-03-12 23:03:58 +0100

    initial commit

---
+++ src/main/java/one/dvrx/bolcomsite/BolcomsiteApplication.java
@@ -0,0 +1,13 @@
+package one.dvrx.bolcomsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolcomsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BolcomsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/one/dvrx/bolcomsite/BolcomsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package one.dvrx.bolcomsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolcomsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
