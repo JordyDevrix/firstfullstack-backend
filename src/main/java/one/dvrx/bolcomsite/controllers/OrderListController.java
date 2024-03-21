e0f8e915fd387e67c664ea8362b6de941e148930
JordyDevrix <2109jordy2241@gmail.com>
2024-03-21 17:09:27 +0100

    small changes

--- src/main/java/one/dvrx/bolcomsite/controllers/OrderListController.java
+++ src/main/java/one/dvrx/bolcomsite/controllers/OrderListController.java
@@ -1,9 +1,19 @@
 package one.dvrx.bolcomsite.controllers;
 
+import one.dvrx.bolcomsite.dao.OrderListDAO;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/orderlist")
 public class OrderListController {
+
+    private OrderListDAO orderListDAO;
+
+    public OrderListController(OrderListDAO orderListDAO) {
+        this.orderListDAO = orderListDAO;
+    }
+
+
+
 }
